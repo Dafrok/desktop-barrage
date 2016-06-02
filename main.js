bdedbad88eddbb4379933cf1d8bc1d5828a11de7
Dafrok <o.o@mug.dog>
2016-06-02 18:40:59 +0800

    optimize

--- main.js
+++ main.js
@@ -9,52 +9,7 @@ app.on('window-all-closed', function() {
     app.quit()
   }
 })
-/*
-class Barrage extends BrowserWindow {
-  constructor(options) {
-    super({
-      width: 0,
-      height: 0,
-      acceptFirstMouse: true,
-      frame: false,
-      useContentSize: true,
-      transparent: true,
-      hidden: true,
-      x: options.screenSize.width,
-      y: 0 | options.screenSize.height * Math.random(),
-      // movable: false,
-      // resizable: false,
-      alwaysOnTop: true,
-      enableLargerThanScreen: true,
-      show: false,
-      hidden: true
-    })
-    this.text = options.text
-    this.showInactive()
-    this.loadURL('file://' + __dirname + '/barrage.html?text=' + this.text)
-    this.go = () => {
-      var width = this.getSize()[0]
-      var position = this.getPosition()
-      this.x = position[0]
-      this.y = position[1]
-      this.barrageInterval = setInterval(() => {
-        try {
-          this.setPosition(this.x, this.y)
-          if (this.x < -width) {
-            this.stop()
-          } else {
-            this.x = this.x - 5
-          }
-        } catch (e) {}
 
-      }, 16.7)}
-    this.stop = () => {
-      clearInterval(this.barrageInterval)
-      this.destroy()
-    }
-  }
-}
-*/
 app.on('ready', function() {
   var electronScreen = electron.screen
   var size = electronScreen.getPrimaryDisplay().workAreaSize
@@ -65,9 +20,13 @@ app.on('ready', function() {
     mainWindow = null
   })
 
-  app.createBarrage = function (text) {
+  app.on('browser-window-focus', function (e) {
+    mainWindow.focus()
+  })
+  app.createBarrage = function (options) {
     new Barrage({
-      text: text,
+      text: options.text,
+      link: options.link,
       screenSize: size
     })
   }
